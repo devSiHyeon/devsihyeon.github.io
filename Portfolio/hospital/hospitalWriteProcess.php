5eec83004b9050e0e2bfd90614800e8d13c4d8d1
dev.SiHyeon <69497828+devSiHyeon@users.noreply.github.com>
2021-04-08 17:37:51 +0900

    Write Process

--- Portfolio/hospital/hospitalWriteProcess.php
+++ Portfolio/hospital/hospitalWriteProcess.php
@@ -29,5 +29,28 @@
                         </script>
                         ";
             }
+
+                  
+    // 게시판 순번 리셋 (AUTO_INCREMENT)
+    // DB의 순번까지 변하기 때문에 사용하지 않음 -> 다음 부터는 $temp_no=1; 을 List에 작성하여 임의 번호를 부여할 것
+        $sql_2 = " 
+                ALTER TABLE hospital 
+                AUTO_INCREMENT=1
+                ";
+
+        $sql_3 = " 
+                SET @COUNT = 0
+                ";
+
+        $sql_4 = " 
+                UPDATE hospital 
+                SET 
+                        idx = @COUNT:=@COUNT+1
+                WHERE idx
+                ";
+
+        $result_2= mysqli_query($db, $sql_2);
+        $result_3= mysqli_query($db, $sql_3);
+        $result_4= mysqli_query($db, $sql_4);
     ?>
             <!-- history.back(); -->
